f86bdb9b03bc5e2614f2d4e1b27eb9cdf06b0ff1
lenochware <lenochware@gmail.com>
2019-06-07 14:04:17 +0200

    +Room

---
+++ src/styx/levels/Room.js
@@ -0,0 +1,179 @@
+var Styx = Styx || {};
+Styx.levels = Styx.levels || {};
+
+Styx.levels.Entrance = class
+{
+	constructor(room, id, pos)
+	{
+		this.room = room;
+		this.id = id;
+		this.side = id;
+		this.pos = pos;
+		this.connected = null;
+	}
+
+	oppositeSide()
+	{
+		switch (this.side) {
+			case 'south': return 'north';
+			case 'north': return 'south';
+			case 'west': return 'east';
+			case 'east': return 'west';
+			default: return 'unknown';
+		}
+	}
+
+	getPos()
+	{
+		return {x: this.pos.x + this.room.x, y: this.pos.y + this.room.y};
+	}
+
+	connect(room)
+	{
+		var en = this.alignRoom(room);
+		this.connected = room;
+		en.connected = this.room;		
+	}
+
+	alignRoom(room)
+	{
+		var en = room.entrances[this.oppositeSide()];
+		if (!en) {
+			throw new Error("Entrance does not exists.");
+		}
+		if ((en.connected && en.connected != this) || this.connected && this.connected != room) {
+			throw new Error("Entrance is already connected.");
+		}
+
+		room.move(this.getPos().x - en.getPos().x, this.getPos().y - en.getPos().y); //nastavi prekryvajici mistnost
+
+		switch(this.side) {
+			case 'north': room.move(0,-1);  break;
+			case 'south': room.move(0, 1);  break;
+			case 'east':  room.move(1, 0);  break;
+			case 'west':  room.move(-1, 0); break;
+		}
+
+		return en;
+	}
+}
+
+Styx.levels.Room = class extends Styx.Rectangle
+{
+	constructor(name)
+	{
+		super(0,0,0,0,{});
+
+		this.name = name;
+		this.assign(0, 0, this.getAttrib('width'), this.getAttrib('height'));
+		this.cells = this.getCells();
+		this.entrances = this._getEntrances();
+	}
+
+	getAttrib(attrib)
+	{
+		return rooms[this.name][attrib];
+	}
+
+	getCells()
+	{
+		var cells = [];
+		var rows = this._chunkSplit(this.getAttrib('cells'), this.width);
+		for(let row of rows) {
+			cells.push(row.split(""));
+		}
+
+		return cells;
+	}
+
+	is(tag)
+	{
+		return (this.getAttrib("tags").indexOf(tag) != -1);
+	}
+
+	/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+  rotate()
+  {
+		this.cells = _.zip(...this.cells.reverse());
+
+		[this.width, this.height] = [this.height, this.width];
+
+		this.entrances = this._getEntrances();
+		return this;
+	}
+
+	_chunkSplit(str, length) {
+	  return str.match(new RegExp('.{1,' + length + '}', 'g'));
+	}
+
+	getCell(x,y)
+	{
+		return this.cells[y][x];
+	}
+
+	findChar(char)
+	{
+		var pos = [];
+		
+		for (var y = 0; y < this.height; y++) {
+			for (var x = 0; x < this.width; x++) {
+				if (this.cells[y][x] == char) pos.push({x:x, y:y});
+			}
+		}
+
+		return pos;
+	}
+
+	_getSide(pos)
+	{
+		if (pos.x == 0) return 'west';
+		if (pos.y == 0) return 'north';
+		if (pos.y == this.height - 1) return 'south';
+		if (pos.x == this.width - 1) return 'east';
+		return false;
+	}
+
+	_getEntrances()
+	{
+		var listEnt = {};
+		var listPos = this.findChar('+');
+		for (let pos of listPos) {
+			var side = this._getSide(pos);
+			if (!side) {
+				console.log(this, listPos);
+				throw new Error("Wrong entrance.");
+			}
+
+			listEnt[side] = new Entrance(this, side, pos);
+		}
+
+		return listEnt;
+	}
+
+	draw(level)
+	{
+		var cor = this.is('corridor');
+
+		for (var y = 0; y < this.height; y++) {
+			for (var x = 0; x < this.width; x++) {
+				var cell = this.cells[y][x];
+				if (cell == ' ') continue;
+				if (cor && cell == '+') cell = '.';
+				//nevykreslovat neconnected doors v mistnostech
+
+				level.setCell({x:this.x + x, y:this.y + y}, 'char', cell);
+			}
+		}
+	}
+
+	freeEntrances()
+	{
+		var list = [];
+		_.each(this.entrances, (en, key) => {if(!en.connected) list.push(en)});
+		return list;
+	}
+
+}
