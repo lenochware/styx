f9f17d819350aeba5feddb3da0810c78827342a5
lenochware <lenochware@gmail.com>
2019-06-13 11:32:20 +0200

    +random: bet(), int(), float(), range(), chances()...

--- src/styx/utils/Random.js
+++ src/styx/utils/Random.js
@@ -7,4 +7,40 @@ Styx.utils.Random = class
 	{
 		return (Math.floor(Math.random() * 101) < percent);
 	}
+
+	bet(p)
+	{
+		return (Math.random() < p);
+	}
+
+	int(max)
+	{
+		return Math.floor(Math.random() * ++max);
+	}
+
+	float(max)
+	{
+		return Math.random() * max;
+	}
+
+	range(min, max)
+	{
+		return this.int(max - min) + min;
+	}
+
+	chances(list)
+	{
+		var sum = _.reduce(list, (memo, num) =>  memo + num, 0);
+		var val = this.float(sum);
+		sum = list[0];
+		for(let i = 0; i < list.length; i++) {
+			if (sum >= val) return i;
+			sum += list[i + 1];
+		}
+	}
+
+	pick(list)
+	{
+		return _.sample(list);
+	}
 }
